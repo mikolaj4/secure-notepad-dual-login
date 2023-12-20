f7230492ef0d8afbc27d87ba48847c6e043eb18d
mikgaw@st.amu.edu.pl <mikgaw@st.amu.edu.pl>
2023-12-20 11:24:49 +0100

    log in activity

---
+++ README.txt
@@ -0,0 +1,7 @@
+Ekran pierwszy:
+	User wybiera czy loguje się hasłem czy fingerprintem. 
+Naciśnięcie logowania hasłem:
+	Następuje przeniesienie do osobnej activity.
+	Musi być powrót z niej.
+	Jeśli jest to pierwsze logowanie hasłem to wyskakuje popup z jego 	utworzeniem.
+	
--- app/src/main/java/com/example/bsm_dual_notatnik/MainActivity.java
+++ app/src/main/java/com/example/bsm_dual_notatnik/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.bsm_dual_notatnik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogInPassword, btnLogInBiometry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogInPassword = findViewById(R.id.btnLogInPassword);
+        btnLogInBiometry = findViewById(R.id.btnLogInBiometry);
+
+        btnLogInPassword.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), LogInPasswordActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
