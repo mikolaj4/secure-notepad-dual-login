24224a8032f358810cd14d3761c7e1535c1baac3
mikgaw@st.amu.edu.pl <mikgaw@st.amu.edu.pl>
2024-01-06 19:14:23 +0100

    dual login done

--- app/src/main/java/com/example/bsm_dual_notatnik/MainActivity.java
+++ app/src/main/java/com/example/bsm_dual_notatnik/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.bsm_dual_notatnik;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
     private static final String SHARED_FILE_NAME = "Notes";
@@ -26,7 +35,65 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
+
+
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)){
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d("MY_LOGS", "App can authenticate using biometrics!");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Log.e("MY_LOGS", "Device is not equipped with biometric hardware!");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Log.e("MY_LOGS", "Biometry currently unavailable!");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Log.e("MY_LOGS", "No fingerprint assigned!");
+                break;
+        }
+
+        BiometricPrompt.PromptInfo promptInfo;
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for your notes")
+                .setSubtitle("Log in using biometric credential")
+                .setNegativeButtonText(" ")
+                .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+                .build();
+
+        BiometricPrompt biometricPrompt;
+        Executor executor;
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback(){
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString){
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(), "Authentication error: " + errString, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+
+                Toast.makeText(getApplicationContext(),"Authentication succeeded!", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), NotesActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAuthenticationFailed(){
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnLogInBiometry.setOnClickListener(view -> biometricPrompt.authenticate(promptInfo));
+
     }
+
 
     private void firstLaunchPasswordSetup(){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_FILE_NAME, MODE_PRIVATE);
@@ -36,4 +103,5 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
     }
+
 }
